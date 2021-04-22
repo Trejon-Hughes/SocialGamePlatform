038c8c42b77b5b4b9a49b69cc14ded72e403fe95
ekkaiden <2003kaiden@gmail.com>
2021-04-21 23:15:43 -0400

    kaiden

--- SocialGamePlatform.Data/Account.cs
+++ SocialGamePlatform.Data/Account.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,7 @@ namespace SocialGamePlatform.Data
 {
     public class Account
     {
+        [Required]
+        public string UserName { get; set; }
     }
 }
---
+++ SocialGamePlatform.Data/Post.cs
@@ -0,0 +1,24 @@
+﻿using Google.Apis.Admin.Directory.directory_v1.Data;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialGamePlatform.Data
+{
+    class Post
+    {
+        [Key]
+        public int PostId { get; set; }
+        [Required]
+        public Guid PoserID { get; set; }
+        [ForeignKey(Account (UserName))]
+        public string PosterUserName { get; set; }
+        [Required]
+        [MinLength(3),MaxLength(240)]
+        public string Text { get; set; }
+    }
+}
