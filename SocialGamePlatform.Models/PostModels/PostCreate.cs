64187e99752a4e9a917f2dbe444645f202066755
ekkaiden <2003kaiden@gmail.com>
2021-04-29 01:53:38 -0400

    kaiden

--- SocialGamePlatform.Models/PostModels/PostCreate.cs
+++ SocialGamePlatform.Models/PostModels/PostCreate.cs
@@ -10,10 +10,16 @@ namespace SocialGamePlatform.Models
     public class PostCreate
     {
         [Required]
+        [MinLength(1, ErrorMessage = "Please enter at least 1 character.")]
+        [MaxLength(400, ErrorMessage = "There are too many characters in this field.")]
+        public string PostName { get; set; }
+        [Required]
         [MinLength(2, ErrorMessage = "Please enter at least 2 characters.")]
-        [MaxLength(5000, ErrorMessage = "There are too many characters in this field.")]
+        [MaxLength(10000, ErrorMessage = "There are too many characters in this field.")]
         public string Text { get; set; }
         [Required]
         public int PostId { get; set; }
+        [Required]
+        public Guid PosterID { get; set; }
     }
 }
--- SocialGamePlatform.Service/PostServices.cs
+++ SocialGamePlatform.Service/PostServices.cs
@@ -1,5 +1,6 @@
 ﻿using SocialGamePlatform.Data;
 using SocialGamePlatform.Models;
+using SocialGamePlatform.Models.PostModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,6 +18,7 @@ namespace SocialGamePlatform.Service
                 new Post()
                 {
                     PosterID = _posterID,
+                    PostName = model.PostName,
                     Text = model.Text,
                     PostId = model.PostId
                 };
@@ -25,6 +27,75 @@ namespace SocialGamePlatform.Service
                 ctx.Posts.Add(entity);
                 return ctx.SaveChanges() == 1;
 
+            }
+        }
+        public IEnumerable<PostListItem> GetPostByName(string post)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                    .Posts
+                    .Where(e => e.PostName.ToLower() == post.ToLower())
+                    .Select(
+                        e =>
+                            new PostListItem
+                            {
+                                PostName = e.PostName,
+                                PostId = e.PostId,
+                                PosterUserName = e.PosterUserName,
+                                Text = e.Text
+                            }
+                        );
+                return query.ToArray();
+            }
+        }
+        public IEnumerable<PostListItem> GetPostByID(int id)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var query =
+                    ctx
+                    .Posts
+                    .Where(e => e.PostId == id)
+                    .Select(
+                        e =>
+                            new PostListItem
+                            {
+                                PostName = e.PostName,
+                                PostId = e.PostId,
+                                PosterUserName = e.PosterUserName,
+                                Text = e.Text
+                            }
+                        );
+                return query.ToArray();
+            }
+        }
+        public bool UpdatePost(PostEdit model)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                    .Posts
+                    .Single(e => e.PostId == model.PostId && e.PosterID == _posterID);
+                entity.PostName = model.PostName;
+                entity.Text = model.Text;
+
+                return ctx.SaveChanges() == 1;
+            }
+        }
+        public bool DeletePost(int postId)
+        {
+            using (var ctx = new ApplicationDbContext())
+            {
+                var entity =
+                    ctx
+                    .Posts
+                    .Single(e => e.PostId == postId && e.PosterID == _posterID);
+                ctx.Posts.Remove(entity);
+
+                return ctx.SaveChanges() == 1;
             }
         }
     }
